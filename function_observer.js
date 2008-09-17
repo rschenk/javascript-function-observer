67236e23449b8165aee84d366122671bf58301f6
Ryan Schenk <rschenk@mbl.edu>
2008-09-17 12:44:27 -0400

    Fixed one line where there was a Prototype call. Now it's entirely in Plain Old Javascript

--- function_observer.js
+++ function_observer.js
@@ -3,8 +3,9 @@
   rschenk@gmail.com
 */
 
-Function.prototype.observe_with = function(observer) {
-  var options = { execute_method: true }; Object.extend(options, arguments[1] || {});
+Function.prototype.pushObserver = function(observer) {
+  var options = { execute_method: true }; 
+  if(arguments[1] && arguments[1].execute_method != undefined ) { options.execute_method = arguments[1].execute_method;  }// Yup, this is kind of a hack
   var __original_method__ = this;  
   var observed_function = function(){
     var observer_return = observer();
